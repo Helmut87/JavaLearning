711ad1dc1e7d11ea9fcc2b318cd0fde715ae88d2
Vyacheslav Plistik <vplistik@bellintegrator.com>
2023-10-19 18:56:42 +0300

    Second homework

---
+++ src/main/java/CelsiusToFahrenheit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ *Напишите Java-программу для преобразования
+ * температуры из Фаренгейта в градусы Цельсия.
+ */
+
+public class CelsiusToFahrenheit {
+
+    static final double COEFFICIENT = 1.8;
+
+    public static void main(String[] args) {
+        Scanner temperature = new Scanner(System.in);
+        temperature.nextLine();
+    }
+}
--- src/main/java/GenerateRandomString.java
+++
@@ -1,7 +0,0 @@
-package main.java;
-
-public class GenerateRandomString {
-    public static void main(String[] args) {
-
-    }
-}
