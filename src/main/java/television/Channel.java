b7a19ebc9e3afe46508a776c94491b352c1c4a72
Vyacheslav Plistik <vplistik@bellintegrator.com>
2023-11-07 17:09:09 +0300

    Correct television task/ Remove unused methods and classes

---
+++ src/main/java/television/Channel.java
@@ -0,0 +1,30 @@
+package television;
+
+public class Channel {
+    private String name;
+    private int channelNumber;
+    private Program program;
+
+    public Channel(String name, int channelNumber, Program program) {
+        this.name = name;
+        this.channelNumber = channelNumber;
+        this.program = program;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChannelNumber() {
+        return channelNumber;
+    }
+
+    public Program getProgram() {
+        return program;
+    }
+
+    @Override
+    public String toString() {
+        return "Канал " + name + " (№" + channelNumber + ")";
+    }
+}
