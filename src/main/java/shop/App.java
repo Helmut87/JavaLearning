49791592420d891fbfae0a94f7fa435dfe4a1ade
Vyacheslav Plistik <vplistik@bellintegrator.com>
2023-11-10 17:09:58 +0300

    Add discount class and correct methods for shopping

--- src/main/java/shop/App.java
+++ src/main/java/shop/App.java
@@ -2,6 +2,7 @@ package shop;
 
 import com.github.javafaker.Faker;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
@@ -9,7 +10,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         ArrayList<Person> people = inputPeopleData();
-        ArrayList<Product> products = generateRandomProducts();
+        ArrayList<Product> products = generateRandomProductsWithDiscountAndWithoutDiscount();
         purchaseProducts(people, products);
         displayShoppingCart(people);
     }
@@ -36,7 +37,7 @@ public class App {
         return people;
     }
 
-    private static ArrayList<Product> generateRandomProducts() {
+    private static ArrayList<Product> generateRandomProductsWithDiscountAndWithoutDiscount() {
         ArrayList<Product> products = new ArrayList<>();
         Locale locale = new Locale("ru_RU");
         Faker faker = new Faker(locale);
@@ -45,18 +46,42 @@ public class App {
             product.setProductName(faker.commerce().productName());
             product.setProductPrice(faker.number().numberBetween(1, 5000));
             products.add(product);
-            System.out.println(products.get(i));
         }
 
+        for (int i = 0; i < 3; i++) {
+            Product product = new Product();
+            product.setProductName(faker.commerce().productName());
+            product.setProductPrice(faker.number().numberBetween(1, 5000));
+
+            // Генерация случайной скидки
+            if (faker.random().nextBoolean()) {
+                int discountAmount = faker.number().numberBetween(1, 99);
+                LocalDate expirationDate = LocalDate.now().plusDays(faker.number().numberBetween(1, 30));
+                DiscountProduct discount = new DiscountProduct(discountAmount, expirationDate);
+                product.setDiscount(discount);
+            }
+
+            products.add(product);
+        }
+        for (Product value : products) {
+            System.out.println(value);
+        }
         return products;
     }
 
     private static void purchaseProducts(ArrayList<Person> people, ArrayList<Product> products) {
         for (Person person : people) {
             for (Product product : products) {
-                if (person.getPersonCash() >= product.getProductPrice()) {
+                int priceToPay = product.getProductPrice();
+                if (product.getDiscount() != null && !product.getDiscount().getExpirationDate().isBefore(LocalDate.now())) {
+                    priceToPay -= product.getDiscount().getDiscountAmount();
+                }
+                if (priceToPay <= 0) {
+                    priceToPay = 1;
+                }
+                if (person.getPersonCash() >= priceToPay) {
                     person.getPersonShoppingCart().add(product);
-                    person.setPersonCash(person.getPersonCash() - product.getProductPrice());
+                    person.setPersonCash(person.getPersonCash() - priceToPay);
                 } else {
                     System.out.println(person.getPersonName() + " не может позволить себе " + product.getProductName());
                 }
--- src/main/java/shop/Product.java
+++ src/main/java/shop/Product.java
@@ -2,17 +2,30 @@ package shop;
 
 import java.util.Objects;
 
+import static org.apache.commons.lang3.math.NumberUtils.isDigits;
+
 public class Product {
     private String productName;
     private int productPrice;
+    private DiscountProduct discount;
+
+    public DiscountProduct getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(DiscountProduct discount) {
+        this.discount = discount;
+    }
 
     public String getProductName() {
         return productName;
     }
 
     public void setProductName(String productName) {
-        if (Objects.equals(productName, "")) {
-            throw new IllegalArgumentException("Название продукта не может быть пустым!");
+        if (Objects.equals(productName, "") || isDigits(productName)) {
+            throw new IllegalArgumentException("Название продукта не может быть пустым и (или) содержать только цифры");
+        } else if (productName.length() < 3) {
+            throw new IllegalArgumentException("Название продукта не может содержать меньше чем 3 символа");
         } else {
             this.productName = productName;
         }
@@ -23,7 +36,11 @@ public class Product {
     }
 
     public void setProductPrice(int productPrice) {
-        this.productPrice = productPrice;
+        if (productPrice <= 0) {
+            throw new IllegalArgumentException("Цена продукта не может быть равной и (или) меньше 0!");
+        } else {
+            this.productPrice = productPrice;
+        }
     }
 
     @Override
@@ -43,9 +60,15 @@ public class Product {
 
     @Override
     public String toString() {
-        return "Продукты: {" +
-                "Наименование продукта: '" + productName + '\'' +
-                ", цена продукта: " + productPrice +
-                '}';
+        StringBuilder sb = new StringBuilder();
+        sb.append("Продукты: {");
+        sb.append("Наименование продукта: '").append(productName).append("'");
+        sb.append(", цена продукта: ").append(productPrice);
+        if (discount != null) {
+            sb.append(", размер скидки: ").append(discount.getDiscountAmount());
+            sb.append(", срок действия скидки: ").append(discount.getExpirationDate());
+        }
+        sb.append("}");
+        return sb.toString();
     }
 }
