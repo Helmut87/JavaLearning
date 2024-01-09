ec169c30f766a492750cbf43a4ff79ddee889b1d
Vyacheslav Plistik <vplistik@bellintegrator.com>
2024-01-09 11:48:41 +0300

    comment corrects for homework010Add

--- src/main/java/PairSampleUse.java
+++ src/main/java/PairSampleUse.java
@@ -1,10 +1,10 @@
 public class PairSampleUse {
     public static void main(String[] args) {
-        Pair<Integer, String> pair = Pair.of(1, "hello");
-        Pair<Integer, String> pair2 = Pair.of(1, "hello");
+        Pair<Integer, String> idAndMessage = Pair.of(1, "hello");
+        Pair<Integer, String> idAndAnotherMessage = Pair.of(1, "hello");
 
-        boolean mustBeTrue = pair.equals(pair2);
-        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode();
+        boolean mustBeTrue = idAndMessage.equals(idAndAnotherMessage);
+        boolean mustAlsoBeTrue = idAndMessage.hashCode() == idAndAnotherMessage.hashCode();
 
         System.out.println(mustBeTrue);
         System.out.println(mustAlsoBeTrue);
--- src/main/java/Sequence.java
+++ src/main/java/Sequence.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Sequence {
     public static int[] filter(int[] array, ByCondition condition) {
         int count = 0;
@@ -19,26 +17,5 @@ public class Sequence {
         }
 
         return result;
-    }
-
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-        // Проверка на четность элемента
-        ByCondition evenCondition = number -> number % 2 == 0;
-        int[] evenResult = filter(array, evenCondition);
-        System.out.println("Элементы, у которых четные значения: " + Arrays.toString(evenResult));
-
-        // Проверка, является ли сумма цифр элемента четным числом
-        ByCondition digitSumEvenCondition = number -> {
-            int sum = 0;
-            while (number > 0) {
-                sum += number % 10;
-                number /= 10;
-            }
-            return sum % 2 == 0;
-        };
-        int[] digitSumEvenResult = filter(array, digitSumEvenCondition);
-        System.out.println("Элементы, у которых сумма цифр четна: " + Arrays.toString(digitSumEvenResult));
     }
 }
