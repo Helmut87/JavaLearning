747492672704a1f13834763a9cd76caf2310b062
Vyacheslav Plistik <vplistik@bellintegrator.com>
2024-01-22 15:56:37 +0300

    Add param tests

--- src/test/java/CarExceptionTests.java
+++ src/test/java/CarExceptionTests.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -15,5 +19,19 @@ public class CarExceptionTests {
     public void testNumberFormatException() {
         CarService carService = new CarService();
         assertThrows(NumberFormatException.class, () -> carService.readCarsFromFile("src/test/resources/cars_negative.txt"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideFileNamesForIOExceptionTest")
+    public void testFileReadingException(String fileName, Class<Exception> expectedException) {
+        CarService carService = new CarService();
+        assertThrows(expectedException, () -> carService.readCarsFromFile(fileName));
+    }
+
+    private static Stream<Arguments> provideFileNamesForIOExceptionTest() {
+        return Stream.of(
+                Arguments.of("non_existent_file.txt", IOException.class),
+                Arguments.of("src/test/resources/cars_negative.txt", NumberFormatException.class)
+        );
     }
 }
