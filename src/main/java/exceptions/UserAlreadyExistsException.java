e080c1e5e2a90d0b82dcc05cca3872985fd58ed6
Vyacheslav Plistik <vplistik@bellintegrator.com>
2024-02-08 14:04:58 +0300

    Attestation 02 initial commit

--- src/main/java/exceptions/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class UserAlreadyExistsException extends Exception {
-    public UserAlreadyExistsException(String id) {
-        super("Пользователь с идентификатором " + id + " уже существует.");
-    }
-}
--- src/main/java/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String id) {
-        super("Пользователь с идентификатором " + id + " не найден.");
-    }
-}
